ce3721161f51ab3ebee6bf701c83a06614078483
Milan Sonani <milans.tagline@gmail.com>
2022-06-13 10:53:36 +0530

    Create school management project

---
+++ classroom/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Student)
+admin.site.register(TakenQuiz)
+
+
---
+++ django_school/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from django.contrib import admin
+from classroom.views import classroom, students, teachers
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('classroom.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', classroom.SignUpView.as_view(), name='signup'),
+    path('accounts/signup/student/', students.StudentSignUpView.as_view(), name='student_signup'),
+    path('accounts/signup/teacher/', teachers.TeacherSignUpView.as_view(), name='teacher_signup'),
+    path('change-password/',auth_views.PasswordChangeView.as_view(
+            template_name='registration/change-password.html',
+            success_url='/'
+        ),name='change_password'),
+]
